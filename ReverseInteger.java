c39c0f65b20e89934c389212216e7df28ff17c79
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-02-16 22:41:23 +0800

    Add files via upload
    Given a signed 32-bit integer x, return x with its digits reversed.
    The reversed x should be in the range of -2^31 and 2^31 - 1.
    If it violates the constraint, then return 0.

---
+++ ReverseInteger.java
@@ -0,0 +1,24 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        /* Reverse a number x */
+        if (x == 0) return 0;
+        int remainder = 0;
+
+        // avoid buffer overflow caused by number reversed greater than Integer.MAX_VALUE(2^31 - 1),
+        // or less than Integer.MIN_VALUE(- 2^31), so declare the output as long (2^63)
+        long output = 0;
+        while (x != 0) {
+            remainder = x % 10;
+            x = x / 10;
+            output = output * 10 + remainder;
+        }
+
+        // check whether the number reversed is greater than Integer.MAX_VALUE
+        // or less than Integer.MIN_VALUE, if so, return 0
+        if (output > Integer.MAX_VALUE || output < Integer.MIN_VALUE) return 0;
+
+        // convert long into int
+        int reverse = (int)output;
+        return (int)reverse;
+    }
+}
