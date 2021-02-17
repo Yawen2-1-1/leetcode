cb72b28b871c13e3333927fbc800ae936041a746
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-02-17 12:26:44 +0800

    Update ReverseInteger.java
    Given a signed 32-bit integer x, return x with its digit reversed.
    The reversed x should be in the range of -2^31 and 2^31 - 1.
    If it is out of the range, then return 0.

--- 7.Reverse Integer/ReverseInteger.java
+++ 7.Reverse Integer/ReverseInteger.java
@@ -1,16 +1,14 @@
-/***************************
- **  Runtime: 1ms          *
- **  Memory Usage: 36.2 MB *
- ***************************/
+/****************************
+ **  Runtime: 1ms          **
+ **  Memory Usage: 36.1 MB **
+ ****************************/
 
 public class ReverseInteger {
     public int reverse(int x) {
-        /* Reverse a number x */
-        if (x == 0) return 0;
+        // avoid buffer overflow caused by number reversed greater than 
+        // Integer.MAX_VALUE(2^31 - 1), or less than Integer.MIN_VALUE(- 2^31),
+        // so declare the output as long (2^63)
         int remainder = 0;
-
-        // avoid buffer overflow caused by number reversed greater than Integer.MAX_VALUE(2^31 - 1),
-        // or less than Integer.MIN_VALUE(- 2^31), so declare the output as long (2^63)
         long output = 0;
         while (x != 0) {
             remainder = x % 10;
@@ -22,8 +20,6 @@ public class ReverseInteger {
         // or less than Integer.MIN_VALUE, if so, return 0
         if (output > Integer.MAX_VALUE || output < Integer.MIN_VALUE) return 0;
 
-        // convert long into int
-        int reverse = (int)output;
-        return (int)reverse;
+        return (int)output;
     }
 }
