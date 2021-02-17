4b6f69ff952ad9dcebadf0ac80905fbb1bf6f566
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-02-17 13:10:31 +0800

    Update and rename TwoSum.java to 1.Two Sum/TwoSum.java
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    Assume each input has exactly one solution, and may not use the same element twice.

--- 1.Two Sum/TwoSum.java
+++ 1.Two Sum/TwoSum.java
@@ -7,8 +7,8 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] output = new int[2]; // return indices of the two numbers
 
-        /* target - nums[i] = sub */
-        /* traverse and find if sub exists in the list */
+        // target - nums[i] = sub
+        // traverse and find if sub exists in the array
         for (int i = 0; i < nums.length; i++) {
             int sub = target - nums[i];
             for (int j = (i+1); j < nums.length; j++) {
