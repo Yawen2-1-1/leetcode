553e25a70a3e8615ba42392e3227d065a259ccb0
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-02-17 12:43:55 +0800

    Add files via upload
    Given a signed 32-bit integer x, return x with its digit reversed.
    The reversed x should be in the range of -2^31 and 2^31 -1.
    If it is out of the range, then return 0.

---
+++ 7.Reverse Integer/leetcode.c
@@ -0,0 +1,18 @@
+
+
+int reverse(int x){
+    // save reversed x in output
+    // avoid buffer overflow: declare output as long int
+    int remainder = 0;
+    long int output = 0;
+    while (x != 0) {
+        remainder = x % 10;
+        x  = x / 10;
+        output = output * 10 + remainder;
+    }
+    
+    // constraint: -2^31 <= x <= 2^31 - 1
+    if (output < INT_MIN || output > INT_MAX) return 0;
+    
+    return (int)output;
+}
