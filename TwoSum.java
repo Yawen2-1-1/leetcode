d03707fd4bf352b5ff6621e1a8eea9a15761c757
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-02-17 13:08:25 +0800

    Add files via upload
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    Assume each input has exactly one solution, and may not use the same element twice.

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+/*********************************
+ **     Runtime: 10ms           **
+ **     Memory Usage: 39.4 MB   **
+ *********************************/
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2]; // return indices of the two numbers
+
+        /* target - nums[i] = sub */
+        /* traverse and find if sub exists in the list */
+        for (int i = 0; i < nums.length; i++) {
+            int sub = target - nums[i];
+            for (int j = (i+1); j < nums.length; j++) {
+                if (nums[j] == sub) {
+                    output[0] = i;
+                    output[1] = j;
+                }
+            }
+        }
+
+        return output;
+    }
+}
